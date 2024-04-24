df21aab42d35dc7bd9302d236a8f3b3112e675ad
Nirendra1 <pratapneerendra198@gmail.com>
2024-04-24 13:57:30 +0530

    commit

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '6c3f86c9fe988d246fdef52c451616d2';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
